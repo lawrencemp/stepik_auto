c5ae6b4284beceb20008acb801c1e687be0dd0e2
lavrentev <loktarmp@gmail.com>
2023-03-03 17:14:11 +0300

    Add files for page object pattern training

---
+++ PageObject/pages/base_page.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+class BasePage:
+    def __init__(self, browser, url, timeout):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
